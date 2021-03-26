77bb636beb608ca84fc3d511992eeb1b6e101d02
Manisha339 <mallu@teksystems.com>
2021-03-26 12:10:22 +0530

    routing

--- test-app/src/app/app-routing.module.ts
+++ test-app/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ListPostComponent } from 'angular-lab/test-app/src/app/user/list-post/list-post.component';
-import { UserFormComponent } from 'angular-lab/test-app/src/app/user/user-form/user-form.component';
+import { ListPostComponent } from './user/list-post/list-post.component';
+import { UserFormComponent } from './user/user-form/user-form.component';
 
 const routes: Routes = [
   {
